8a000d3468e8ee45d85974e8fa55f5f52914d2f1
Anakin Childerhose <anakin@childerhose.ca>
2024-04-02 11:43:20 -0400

    I have vim navigation working

---
+++ plugin/navi.lua
@@ -0,0 +1,96 @@
+
+local debug = true
+
+do -- Initial checks
+    if loaded_navigator ~= nil and debug == false then
+        vim.notify('vim-tmux-navi-lua already loaded', vim.log.levels.WARN)
+        return
+    end
+
+    if vim.o.compatible then
+        vim.notify(
+            'vim-tmux-navi-lua compatible mode not supported',
+            vim.log.levels.ERROR
+        )
+        return
+    end
+
+    -- TODO: Figure out lowest supported version number
+end
+
+loaded_navigator = 1
+
+
+
+local function num_of_tmux_panes ()
+    return tonumber(vim.fn.system("tmux list-panes -F '#{pane_id}' | wc -l") or 0)
+end
+
+-------------
+--  Navigates windows in Vim or Tmux.
+--  @param args.key The key associated with the direction to navigate to
+--  @param args.vim_mode Boolean to determine whether the navigation is vim or tmux
+function Navigate (args)
+
+    assert(args.key ~= nil, "no key provided")
+    assert(string.find("hjklp", args.key), "key not valid")
+
+    -- vim navigation
+    if args.vim_mode or num_of_tmux_panes() == 1 then
+        local status, err = pcall(
+            vim.api.nvim_command, "wincmd " .. args.key
+        )
+        if not status then
+            vim.notify(
+                'vim_navigate: failed to call wincmd. ' .. tostring(err),
+                vim.log.levels.error
+            )
+        end
+        return
+    end
+
+    -- try vim navigation first and test if movement happened
+    local winnr_prev = vim.fn.winnr()
+    Navigate { key = args.key, vim_mode = true }
+    if winnr_prev ~= vim.fn.winnr() then
+        return -- Movement in vim occured
+    end
+
+    -- tmux navigation
+    vim.notify("TMUX TMUX")
+end
+
+-- Set vim_mode based on $TMUX
+local tmux_string = os.getenv("TMUX")
+local vim_mode = false
+if tmux_string == '' or tmux_string == nil then
+    vim_mode = true
+end
+
+local key_to_direction = {
+    h = 'left',
+    j = 'bottom', -- tmux specific naming
+    k = 'top', -- tmux specific naming
+    l = 'right',
+    p = 'prev'
+}
+
+for key, direction in pairs(key_to_direction) do
+    vim.cmd(
+        "command! Navigate" .. direction ..
+        " lua Navigate { key = '" .. key .. "'," ..
+        " vim_mode = " .. tostring(vim_mode) .. " }"
+    )
+end
+
+-- Create commands for all directions
+-- vim.cmd("command! NavigateLeft lua Navigate { direction = 'h', vim_mode = "
+--     .. tostring(vim_mode) .. " }")
+-- vim.cmd("command! NavigateDown lua Navigate { direction = 'j', vim_mode = "
+--     .. tostring(vim_mode) .. " }")
+-- vim.cmd("command! NavigateUp lua Navigate { direction = 'k', vim_mode = "
+--     .. tostring(vim_mode) .. " }")
+-- vim.cmd("command! NavigateRight lua Navigate { direction = 'l', vim_mode = "
+--     .. tostring(vim_mode) .. " }")
+-- vim.cmd("command! NavigatePrev lua Navigate { direction = 'p', vim_mode = "
+--     .. tostring(vim_mode) .. " }")
